853b0f244fa5b2df2a570fd21c6f18f7af3e1bee
brhelm <brhelm77@gmail.com>
2017-09-28 14:26:19 -0700

    bodycontent, inboxsection, inboxsearch additions

--- client/src/components/coachpage/bodycontent/requestsection/InboxSection.js
+++ client/src/components/coachpage/bodycontent/requestsection/InboxSection.js
@@ -1,13 +1,12 @@
 import React from "react"; 
 import "./InboxSection.css";
-import InboxSearch from "./inboxsearch/InboxSearch"
 
 const InboxSection = () => (
 	<div id="inboxSection">
 		<h3 className="text-center">Pending VOD review Requests</h3>
 		<h4><u>Select a VOD and start review</u></h4>
 		
-		<InboxSearch />
+		<inbox />
 		
 
 		
