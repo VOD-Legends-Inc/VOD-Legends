53ca44f6046736b1e531df1024ee3bb1aef2a447
Joseph Calabrese <j.calabrese091@gmail.com>
2017-10-04 14:30:04 -0700

    fix routes

--- client/src/utils/coachAPI.js
+++ client/src/utils/coachAPI.js
@@ -2,24 +2,19 @@ import axios from "axios";
 
 export default {
   // Gets all coaches
-<<<<<<< HEAD
-  getCoach: function() {
-    return axios.get("/api/coach");
-=======
   getCoaches: function() {
     return axios.get("/api/coaches");
->>>>>>> 316ff4e43c2e333be451bdd4590782c3fd8d6773
   },
   // Gets the coach with the given id
   getCoach: function(id) {
-    return axios.get("/api/coach/" + id);
+    return axios.get("/api/coaches/" + id);
   },
   // Deletes the coach with the given id
   deleteCoach: function(id) {
-    return axios.delete("/api/coach/" + id);
+    return axios.delete("/api/coaches/" + id);
   },
   // Saves a coach to the database
   saveCoach: function(coachData) {
-    return axios.post("/api/coach", coachData);
+    return axios.post("/api/coaches", coachData);
   }
 };
--- client/src/utils/requestAPI.js
+++ client/src/utils/requestAPI.js
@@ -2,29 +2,21 @@ import axios from "axios";
 
 export default {
   // Gets all requests
-<<<<<<< HEAD
-  getRequest: function() {
-    return axios.get("/api/request");
-=======
+
   getRequests: function() {
     return axios.get("/api/requests");
->>>>>>> 316ff4e43c2e333be451bdd4590782c3fd8d6773
-  },
+},
   // Gets the request with the given id
   getRequest: function(id) {
-    return axios.get("/api/request/" + id);
+    return axios.get("/api/requests/" + id);
   },
   // Deletes the request with the given id
   deleteRequest: function(id) {
-    return axios.delete("/api/request/" + id);
+    return axios.delete("/api/requests/" + id);
   },
   // Saves a request to the database
-<<<<<<< HEAD
-  saveRequest: function(coachData) {
-    return axios.post("/api/request", requestData);
-=======
+
   saveRequest: function(requestData) {
     return axios.post("/api/requests", requestData);
->>>>>>> 316ff4e43c2e333be451bdd4590782c3fd8d6773
   }
 };
