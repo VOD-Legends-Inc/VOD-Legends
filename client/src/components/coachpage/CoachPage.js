db5e34dc58b2b8f376c3483c122af2e7c7a8144d
brhelm <brhelm77@gmail.com>
2017-10-03 22:18:51 -0700

    profile buttons

--- client/src/components/coachpage/CoachPage.js
+++ client/src/components/coachpage/CoachPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react"; 
 import "./CoachPage.css";
 import Navbar from "./navbar/Navbar";
 import BodyContent from "./bodycontent/BodyContent";
