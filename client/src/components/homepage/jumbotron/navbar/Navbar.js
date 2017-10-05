155c252f6a8e388d37274f04718c0f1d9cb94fb1
azianpinoy <jdechavez23@gmail.com>
2017-10-05 14:13:33 -0700

    fixed homepage buttons

--- client/src/components/homepage/jumbotron/navbar/Navbar.js
+++ client/src/components/homepage/jumbotron/navbar/Navbar.js
@@ -17,24 +17,16 @@ class Navbar extends Component {
 
     signupClick = event => {
         alert("this worked");
-        this.setState({signupModalVisible: true});
     }
 
-    signupCancel = event => {
+    coachClick = event => {
         alert("this worked");
-        this.setState({signupModalVisible: false});
     }
-
 
     loginClick = event => {
         alert("this worked");
-        this.setState({loginModalVisible: true});
     }
 
-    loginCancel = event => {
-        alert("this worked");
-        this.setState({loginModalVisible: false});
-    }
 
     handleInputChange = event => {
         const {name, value} = event.target;
@@ -57,68 +49,20 @@ class Navbar extends Component {
 
                     <div className="col-sm-6 col-md-6 col-lg-6">
                         <ul className="nav navbar-nav navbar-right">
-              
+                            
+                            <li>
+
+                                <button id="becomeCoachButton" onClick={this.coachClick}>Become a Coach</button>
+                                
+                                
+
+                            </li>
+
                             <li>
 
                                 <button id="signupModalButton" onClick={this.signupClick}>Sign Up</button>
                                 
-                                {
-                                    this.state.signupModalVisible
-                                    ?
-                                    <div id="signupModal">
-
-                                        <form>
-
-                                            <span id="closeSignupModal" className="close" onClick={this.signupCancel}>×</span>
-
-                                            <div className="container">
-
-                                                <h3 className="center-block">vodLegends Sign Up</h3>
-
-                                           
-
-                                                <label><b>First Name</b></label><br/>
-                                                <input type="text" value={this.state.firstName} onChange={this.handleInputChange} name="firstName" placeholder="Enter First Name" required/><br/>
-                                                
-                                                <label><b>Last Name</b></label><br/>
-                                                <input type="text" value={this.state.lastName} onChange={this.handleInputChange} name="lastName" placeholder="Enter Last Name" required /><br/>
-                                               
-                                       
-
-                                          
-                                                <label><b>Username</b></label><br/>
-                                                <input type="text" value={this.state.userName} onChange={this.handleInputChange} name="userName" placeholder="Enter Username" id="userNameInput" required /><br/>
-                                                
-                                          
-
-                                  
-                                                <label><b>Email</b></label><br/>
-                                                <input type="text" value={this.state.email} onChange={this.handleInputChange} name="email" placeholder="Enter Email" id="emailInput" required /><br/>
-                                          
-
-                                                <label><b>Password</b></label><br/>
-                                                <input type="password" value={this.state.password} onChange={this.handleInputChange} name="password" placeholder="Enter Password" id="passwordInput" required /><br/>
-                                          
-
-                                                <label><b>Repeat Password</b></label><br/>
-                                                <input type="password" value={this.state.repeatPW} onChange={this.handleInputChange} name="repeatPW" placeholder="Repeat Password" id="repeatInput" required /><br/>                     
-
-                                                <div className="center-block">
-                                                    
-                                                    <button type="submit" >Sign Up</button>
-
-                                                    <button type="button" onClick={this.signupCancel}>Cancel</button>
-
-                                                </div>
-
-                                            </div>
-
-                                        </form>
-
-                                    </div>
-                                    : null
-                                }
-
+                                
 
                             </li>
                             
@@ -126,44 +70,7 @@ class Navbar extends Component {
 
                                 <button id="loginModalButton" onClick={this.loginClick}>Login</button>
 
-                                {
-                                    this.state.loginModalVisible
-                                    ?
-                                    <div id="loginModal">
-                                        
-                                        <form>
-
-                                            <span id="closeLoginModal" className="close" onClick={this.loginCancel}>×</span>
-
-                                            <div className="container">
-
-                                                <h3 className="mcenter-block">MyStorya Login</h3>
-                                                
-                                                
-                                                        <label><b>Username</b></label><br/>
-                                                        <input type="text" value={this.state.loginUsername} onChange={this.handleInputChange} placeholder="Enter Username" id="userNameLogin" name="loginUsername" required /><br/>
-                                               
-                                                
-                                              
-                                                        <label><b>Password</b></label><br/>
-                                                        <input type="password" value={this.state.loginPassword} onChange={this.handleInputChange} name="loginPassword"  placeholder="Enter Password" id="passwordLogin" required />
-                                                
-
-                                                <div className="center-block">
-
-                                                    <button type="submit">Login</button>
-                                                    
-                                                    <button type="button" onClick={this.loginCancel}>Cancel</button>
-
-                                                </div>
-                                            
-                                            </div>
-
-                                        </form>
-
-                                    </div>
-                                    : null
-                                }
+                                
 
                             </li>
 
