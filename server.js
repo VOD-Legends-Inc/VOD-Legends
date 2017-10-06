91f9a261bc0744d22b91d9d6dad98510aad2d88b
Joseph Calabrese <j.calabrese091@gmail.com>
2017-10-05 22:34:43 -0700

    update

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(routes);
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.mongolab-round-77838 || "mongodb://localhost/vodLegends",
+  process.env.MONGODB_URI || "mongodb://localhost/vodLegends",
   {
     useMongoClient: true
   }
