df40ef8b38f206965ba36421fec84672fc4ff583
Joseph Calabrese <j.calabrese091@gmail.com>
2017-11-07 18:51:21 -0800

    update homepage

--- client/src/components/homepage/jumbotron/Jumbotron.js
+++ client/src/components/homepage/jumbotron/Jumbotron.js
@@ -5,19 +5,17 @@ import "./Jumbotron.css";
 const Jumbotron = () => (
   <div className="jumbotron">
     <Navbar/><br/><br/><br/><br/>
-
-    <Navbar />
-
-    <img className="center-block" src="./images/vodlg.png" alt="vodlg" />
     
-    <h1 className="text-center textyellow textborder"> Improve Your Skills! </h1>
-	<p className="text-center textyellow textborder">
-      Submit your VOD today <br/>
-      and receive Diamond level review and analysis.
-    </p>
-    {/*<img alt="featureImage" className="triImage2 center-block" src="./images/challenger.png" />*/}
-    <img alt="getstartednow" className="getstarted center-block" src="./images/started.png" onClick={this.signupClick} />
+    <h1 className="text-center textgold textborder"> Improve Your Skills! </h1>
+  	 
+     <p className="text-center textgold textborder">
+        Submit your VOD today <br/>
+        and receive Diamond level review and analysis.
+      </p>
 
+      <a href="/studentSignup">
+        <img alt="getstartednow" className="getstarted center-block" src="./images/started.png" onClick={this.signupClick} />
+      </a>
   </div>
 );
 
